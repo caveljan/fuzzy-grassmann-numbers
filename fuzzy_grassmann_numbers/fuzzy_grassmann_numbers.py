4920eb40e43b64d6a1df5f175a06a436c26ca690
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2018-12-13 09:04:30 +0200

    test: simple_add()

--- fuzzy_grassmann_numbers/fuzzy_grassmann_numbers.py
+++ fuzzy_grassmann_numbers/fuzzy_grassmann_numbers.py
@@ -4,17 +4,17 @@ class GrassmannNumber():
         print(args)
 
 
-# def GrassmannAdd(first_number, second_number):
-#     print(f'The first GrassmannNumber: {first_number.args}')
-#     print(f'The second GrassmannNumber: {second_number.args}')
-#     first_number = first_number.args
-#     second_number = second_number.args
-#     sum_number = []
-#     if len(first_number) == len(second_number):
-#         for i in range(len(first_number)):
-#             sum = first_number[i] + second_number[i]
-#             sum_number.append(sum)
-#     return sum_number
+def GrassmannAdd(first_number, second_number):
+    print(f'The first GrassmannNumber: {first_number.args}')
+    print(f'The second GrassmannNumber: {second_number.args}')
+    first_number = first_number.args
+    second_number = second_number.args
+    sum_number = []
+    if len(first_number) == len(second_number):
+        for i in range(len(first_number)):
+            sum = first_number[i] + second_number[i]
+            sum_number.append(sum)
+    return sum_number
 
 
 # class FuzzyNumber():
--- test/fgn.py
+++ test/fgn.py
@@ -1,5 +1,10 @@
-# def func(x):
-#     return x + 1
+from .context import fgn
 
-# def test_answer():
-#     assert func(1) == 2
+
+def simple_add():
+    a = fgn.GrassmannNumber(2, 3, 4)
+    b = fgn.GrassmannNumber(2, 1, 0)
+    return fgn.GrassmannAdd(a, b)
+
+def test_simple_add():
+    assert simple_add() == [4, 4, 4]
